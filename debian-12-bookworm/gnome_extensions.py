cfa43170d447c2b2d1710e1908ceb25751c86fa1
Callum <github@calobyte.net>
2025-03-24 21:06:53 +0000

    fix spelling

--- debian-12-bookworm/gnome_extensions.py
+++ debian-12-bookworm/gnome_extensions.py
@@ -4,4 +4,4 @@ if get_arg("--dock"):
     run_command(
         "wget -c https://github.com/micheleg/dash-to-dock/releases/latest/download/dash-to-dock@micxgx.gmail.com.zip -O ./dash-to-dock.zip"
     )
-    run_command("gnome-extensions install ./dask-to-dock.zip")
+    run_command("gnome-extensions install ./dash-to-dock.zip")

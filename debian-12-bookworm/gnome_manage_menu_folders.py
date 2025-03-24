47ca3e3738bc77d158bf12713bf0eac378988926
Callum <github@calobyte.net>
2025-03-24 19:36:02 +0000

    start testing folder items code

--- debian-12-bookworm/gnome_manage_menu_folders.py
+++ debian-12-bookworm/gnome_manage_menu_folders.py
@@ -1,27 +1,50 @@
 from _common import *
 
-add_folders = [folder.replace("--add-folder=", "").split(":") for folder in args if folder.startswith("--add-folder=")]
-add_folder_items = [folder.replace("--add-folder-items=", "").split(":") for folder in args if folder.startswith("--add-folder-items=")]
+add_folders = [
+    folder.replace("--add-folder=", "").split(":")
+    for folder in args
+    if folder.startswith("--add-folder=")
+]
+add_folder_items = [
+    folder.replace("--add-folder-items=", "").split(":")
+    for folder in args
+    if folder.startswith("--add-folder-items=")
+]
 
 
 def add_folder_to_menu(folder_name, folder_id):
-    reg_folders = run_command('gsettings get org.gnome.desktop.app-folders folder-children', True)
-    reg_folders = reg_folders.replace("[", "").replace("]", "").replace("'", "").replace("\n","").split(", ")
+    reg_folders = run_command(
+        "gsettings get org.gnome.desktop.app-folders folder-children", True
+    )
+    reg_folders = (
+        reg_folders.replace("[", "")
+        .replace("]", "")
+        .replace("'", "")
+        .replace("\n", "")
+        .split(", ")
+    )
 
     if folder_id not in reg_folders:
-        run_command(f'gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/{folder_id}/ name {folder_name}')
-        run_command(f'gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/{folder_id}/ apps "[]"')
+        run_command(
+            f"gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/{folder_id}/ name {folder_name}"
+        )
+        run_command(
+            f'gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/{folder_id}/ apps "[]"'
+        )
 
         reg_folders.append(folder_id)
-        folders_string = ", ".join([f"'{folder}'" for folder in  reg_folders])
-        run_command(f'gsettings set org.gnome.desktop.app-folders folder-children "[ {folders_string} ]"')
+        folders_string = ", ".join([f"'{folder}'" for folder in reg_folders])
+        run_command(
+            f'gsettings set org.gnome.desktop.app-folders folder-children "[ {folders_string} ]"'
+        )
         log(f"Gnome Menu Folder {folder_name}:{folder_id} created.")
     else:
         log(f"{folder_name}:{folder_id} already registered.")
 
 
-def add_items_to_folder(folder_id, items_arr):
-    pass
+def add_items_to_folder(folder_id, folder_items):
+    print(folder_id)
+    print(folder_items)
 
 
 for folder in add_folders:
@@ -31,6 +54,10 @@ for folder in add_folders:
 
 for folder in add_folder_items:
     [folder_id, folder_items] = folder
-    folder_items = ", ".join([f"'{folder_item.lstrip().rstrip()}'" for folder_item in folder_items.split(",")])
+    folder_items = ", ".join(
+        [
+            f"'{folder_item.lstrip().rstrip()}'"
+            for folder_item in folder_items.split(",")
+        ]
+    )
     add_items_to_folder(folder_id, folder_items)
-
--- debian-12-bookworm/gnome_tidy_menu.py
+++ debian-12-bookworm/gnome_tidy_menu.py
@@ -1,8 +1,53 @@
 from _common import *
-from gnome_manage_menu_folders import add_folder_to_menu
+from gnome_manage_menu_folders import add_folder_to_menu, add_items_to_folder
 
 if get_arg("--tidy-menu"):
-    add_folder_to_menu('Accessories' ,'accessories')
-    add_folder_to_menu('Utils' ,'utils')
-    add_folder_to_menu('UI' ,'ui')
-    add_folder_to_menu('System' ,'system')
+    add_folder_to_menu("Accessories", "accessories")
+    add_folder_to_menu("Utils", "utils")
+    add_folder_to_menu("UI", "ui")
+    add_folder_to_menu("System", "system")
+
+    add_items_to_folder(
+        "utils",
+        [
+            "org.gnome.Characters.desktop",
+            "org.gnome.font-viewer.desktop",
+            "org.gnome.Cheese.desktop",
+            "com.gexperts.Tilix.desktop",
+        ],
+    )
+
+    add_items_to_folder(
+        "accessories",
+        [
+            "org.gnome.TextEditor.desktop",
+            "org.gnome.FileRoller.desktop",
+            "org.gnome.SoundRecorder.desktop",
+            "org.gnome.eog.desktop",
+            "org.gnome.Calendar.desktop",
+            "org.gnome.Evince.desktop",
+            "org.gnome.clocks.desktop",
+            "org.gnome.Contacts.desktop",
+            "simple-scan.desktop",
+            "org.gnome.Calculator.desktop",
+        ],
+    )
+
+    add_items_to_folder(
+        "system",
+        [
+            "org.gnome.DiskUtility.desktop",
+            "org.gnome.seahorse.Application.desktop",
+            "nm-connection-editor.desktop",
+            "org.gnome.baobab.desktop",
+            "org.gnome.Logs.desktop",
+            "software-properties-gtk.desktop",
+            "im-config.desktop",
+            "org.gnome.Settings.desktop",
+            "gnome-system-monitor.desktop",
+        ],
+    )
+
+    add_items_to_folder(
+        "ui", ["org.gnome.tweaks.desktop", "org.gnome.Extensions.desktop"]
+    )

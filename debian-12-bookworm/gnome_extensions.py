8d0c1e849efd1ea5dc61bb9dfbc78724b5abebb4
Callum <github@calobyte.net>
2025-03-24 21:16:16 +0000

    update dash to dock integration

--- debian-12-bookworm/gnome_extensions.py
+++ debian-12-bookworm/gnome_extensions.py
@@ -5,3 +5,6 @@ if get_arg("--dock"):
         "wget -c https://github.com/micheleg/dash-to-dock/releases/latest/download/dash-to-dock@micxgx.gmail.com.zip -O ./dash-to-dock.zip"
     )
     run_command("gnome-extensions install ./dash-to-dock.zip")
+    run_command(
+        "unzip ./dash-to-dock.zip -d $HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
+    )

47486ec8cec70cc50c9cbee22fc024c08a51fb70
Callum <github@calobyte.net>
2025-03-24 21:05:39 +0000

    dock init

---
+++ debian-12-bookworm/gnome_extensions.py
@@ -0,0 +1,7 @@
+from _common import *
+
+if get_arg("--dock"):
+    run_command(
+        "wget -c https://github.com/micheleg/dash-to-dock/releases/latest/download/dash-to-dock@micxgx.gmail.com.zip -O ./dash-to-dock.zip"
+    )
+    run_command("gnome-extensions install ./dask-to-dock.zip")

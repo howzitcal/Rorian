d0f3d91704651331b27a01771c9dc4857bc84e7a
Callum <github@calobyte.net>
2025-03-25 10:04:12 +0000

    fix extension and compile

--- src/debian_12_bookworm/gnome_extensions.py
+++ src/debian_12_bookworm/gnome_extensions.py
@@ -5,7 +5,7 @@ if get_arg("--dock"):
         "wget -c https://github.com/micheleg/dash-to-dock/releases/latest/download/dash-to-dock@micxgx.gmail.com.zip -O ./dash-to-dock.zip"
     )
     run_command(
-        "unzip ./dash-to-dock.zip -qq -d $HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
+        "unzip -o -q ./dash-to-dock.zip -d $HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
     )
     run_command("gnome-extensions enable dash-to-dock@micxgx.gmail.com")
 # https://unsplash.com/photos/DjlKxYFJlTc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzQyODQ2MTA2fA&force=true&w=2400

df05685f328b42b28ae2faa6e48e544a7a8d69b5
Callum <github@calobyte.net>
2025-03-25 11:02:17 +0000

    test extensions

--- src/debian_12_bookworm/gnome_extensions.py
+++ src/debian_12_bookworm/gnome_extensions.py
@@ -1,11 +1,9 @@
 from _common import *
 
-run_command("mkdir -p $HOME/.local/share/gnome-shell/extensions/")
 
 if get_arg("--dock"):
     run_command(
-        "wget -c https://github.com/micheleg/dash-to-dock/releases/latest/download/dash-to-dock@micxgx.gmail.com.zip -O ./dash-to-dock.zip"
+        'gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "dash-to-dock@micxgx.gmail.com"InstallRemoteExtension "dash-to-dock@micxgx.gmail.com"'
     )
-    run_command("gnome-extensions install ./dash-to-dock.zip")
-    run_command("gnome-extensions enable dash-to-dock@micxgx.gmail.com")
+
 # https://unsplash.com/photos/DjlKxYFJlTc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzQyODQ2MTA2fA&force=true&w=2400

beb5234e23b9c649bc319b9ce5ea1b3d0d5941ae
Callum <github@calobyte.net>
2025-03-25 09:53:03 +0000

    add quiet to unzip

--- src/debian_12_bookworm/gnome_extensions.py
+++ src/debian_12_bookworm/gnome_extensions.py
@@ -6,7 +6,7 @@ if get_arg("--dock"):
     )
     run_command("gnome-extensions install ./dash-to-dock.zip")
     run_command(
-        "unzip ./dash-to-dock.zip -d $HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
+        "unzip ./dash-to-dock.zip -qq -d $HOME/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com"
     )
     run_command("gnome-extensions enable dash-to-dock@micxgx.gmail.com")
 # https://unsplash.com/photos/DjlKxYFJlTc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzQyODQ2MTA2fA&force=true&w=2400

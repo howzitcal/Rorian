cf49d8ae32366c018b7fdc0b1a05eace2772d04d
Callum <github@calobyte.net>
2025-03-25 14:46:42 +0000

    update extension command

--- src/debian_12_bookworm/gnome_extensions.py
+++ src/debian_12_bookworm/gnome_extensions.py
@@ -3,7 +3,7 @@ from _common import *
 
 if get_arg("--dock"):
     run_command(
-        'gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "dash-to-dock@micxgx.gmail.com"'
+        'gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "dash-to-dock@micxgx.gmail.com" || true'
     )
 
 # https://unsplash.com/photos/DjlKxYFJlTc/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzQyODQ2MTA2fA&force=true&w=2400
